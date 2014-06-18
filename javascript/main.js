7a9cd4cea7c41be9524377efc0fe7b8d89e2066f
ardian bahtiarsyah <ardian.bahtiarsyah@gmail.com>
2014-06-18 14:08:54 +0700

    cleaned up some more

--- javascript/main.js
+++ javascript/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	    $target = $(target); 
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
-	    }, 400, 'swing', function () {
+	    }, 1000, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
